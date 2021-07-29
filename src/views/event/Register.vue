264876b6fc9e77dbda121caa3d3b42d943b6ce11
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-07-29 12:16:05 +0700

    1.3 add button in Register.vue

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,23 @@
 <template>
   <p>Regstration form here</p>
+  <button @click="register">Register</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  methods: {
+    register() {
+      // Assuming successful API call to register them
+
+      //Set a flash message to appear on the next page loaded whicjh says
+      //'You are successfully registered for ' + this.event.title
+
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
