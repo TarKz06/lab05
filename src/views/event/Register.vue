3f07e1405575f54fa826092d2b2ab1e0055874e7
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-07-29 12:25:03 +0700

    1.4 add inject and setTimeOut in register

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -6,12 +6,19 @@
 <script>
 export default {
   props: ['event'],
+  inject: ['GStore'],
   methods: {
     register() {
       // Assuming successful API call to register them
-
       //Set a flash message to appear on the next page loaded whicjh says
       //'You are successfully registered for ' + this.event.title
+      this.GStore.flashMessage =
+        'You are successfully registered for ' + this.event.title
+      setTimeout(() => {
+        //After 3 second remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+      // Set a flash message to appear on the next page loaded which says
 
       this.$router.push({
         name: 'EventDetails',
